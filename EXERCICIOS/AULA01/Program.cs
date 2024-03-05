c5080c26cbef75c310adc94ca6456a51cc30747a
Alexandre José Ribeiro <415709@unoesc.lan>
2024-03-04 21:09:43 -0300

    Tipos numéricos

--- EXERCICIOS/AULA01/Program.cs
+++ EXERCICIOS/AULA01/Program.cs
@@ -29,3 +29,36 @@ Console.WriteLine("Qual é o seu nome?");
 nome = Console.ReadLine();
 string resultado = $"Oi, {nome}! Pare de jaguarice!";
 Console.WriteLine(resultado);
+
+// Comentário de marcação 240304
+
+// Substituindo conteúdo de strings
+string nomeCompleto = "Leonardo M Zonta";
+nomeCompleto =
+    nomeCompleto.Replace("M", "Zonta");
+Console.WriteLine(nomeCompleto);
+
+// Comparação de strings
+bool isNomeEqual = (nomeCompleto == "mc zontinha");
+
+bool isNomeEqual2 =
+      string.Equals(nomeCompleto, "Leonardo M Zonta");
+
+    Console.WriteLine($"Primeiro resultado: {isNomeEqual}");
+    Console.WriteLine($"Segundo resultado: {isNomeEqual2}");
+
+// TIPOS NUMÉRICOS
+/*
+    sbyte: Armazena valores entre - 128 e 127
+    short: Armazena entre -32, 768 e 32, 767
+    int: Armazena valores entre -2, 147,483, 648 e
+                                -2, 147, 483, 647
+    long:  Armazena valores entre
+           -9,223,372,036,854,775,808 e
+           -9,223,372,036,854,775,807
+*/
+
+float myFloat = 10.0f;
+double myDouble = 5d;
+decimal myDecimal = 7m;
+int myInteger = 0;
--- EXERCICIOS/AULA01/obj/Debug/net8.0/240219_01.AssemblyInfo.cs
+++ EXERCICIOS/AULA01/obj/Debug/net8.0/240219_01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("240219_01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bcab3de09f82ab1ec8a6b0959292847c9c86da0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a031fa021ba32a8758c43a04fa73ff189efa08c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("240219_01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("240219_01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
