6038afede35190df46229760d1dea5dc813d6132
Alexandre José Ribeiro <415709@unoesc.lan>
2024-03-11 21:58:42 -0300

    OPERADORES_ARITMETICOS

---
+++ ATIVIDADES/OPERADORES_ARITMETICOS/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o operador aritmético desejado (+, -, *, /): ");
+        char operador = Console.ReadLine()[0];
+
+        Console.WriteLine("Tabuada de " + operador + ":");
+
+        for (int i = 1; i <= 9; i++)
+        {
+            for (int j = 1; j <= 9; j++)
+            {
+                int resultado = 0;
+
+                switch (operador)
+                {
+                    case '+':
+                        resultado = i + j;
+                        break;
+                    case '-':
+                        resultado = i - j;
+                        break;
+                    case '*':
+                        resultado = i * j;
+                        break;
+                    case '/':
+                        resultado = i / j;
+                        break;
+                    default:
+                        Console.WriteLine("Operador inválido.");
+                        return;
+                }
+
+                Console.Write($"{i} {operador} {j} = {resultado}\t");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
